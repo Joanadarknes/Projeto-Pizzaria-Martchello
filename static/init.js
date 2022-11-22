20af0c9696f28b87dce9c0e80b202f806e11b71e
joana <joanadarknes2233@gmail.com>
2022-11-22 01:13:09 -0300

    Adicionando bd e correções

---
+++ static/init.js
@@ -0,0 +1,11 @@
+// Such info must be obtained from Firebase Project, on firebase console
+var firebaseConfig = {
+  apiKey: "AIzaSyBEBZ4DXNRFqJ7KCmHanT7btjmNXmv_sdM",
+  authDomain: "pizzaria-d676d.firebaseapp.com",
+  projectId: "pizzaria-d676d",
+  storageBucket: "pizzaria-d676d.appspot.com",
+  messagingSenderId: "431311151237",
+  appId: "1:431311151237:web:35e412ef9091301ee4c9ec"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
